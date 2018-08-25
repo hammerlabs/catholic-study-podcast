8f9f32aa1c80fbedef036a19fcd59dc775368581
Kevin Hammer <kevinhammer@KHAMMERs-MacBook-Pro.local>
2018-08-25 12:27:50 -0700

    updates to procedure for creating images and mp3s

--- gulp-tasks/mp3.js
+++ gulp-tasks/mp3.js
@@ -5,6 +5,8 @@ import path from "path";
 import ID3Writer from "browser-id3-writer";
 import mp3Duration from "mp3-duration";
 
+var exec = require("child_process").exec;
+
 gulp.task("mp3-read-tags", function() {
   const dir = "./site/content/episode";
   fs.readdir(dir, function(err, items) {
@@ -24,7 +26,7 @@ function readEpisodeID3(mdfile) {
 }
 
 gulp.task("mp3-set-size", async function() {
-  const dir = "./site/content/episode";
+  const dir = "./site/content/bible-study";
   // read all files in the content/episode
   fs.readdir(dir, async function(err, items) {
     if (err) {
@@ -35,9 +37,10 @@ gulp.task("mp3-set-size", async function() {
       var content = fs.readFileSync(path.join(dir, items[i]), "utf-8");
       var tomlString = content.split("+++")[1];
       var tdata = toml.parse(tomlString);
-      const stats = fs.statSync("./mp3/" + tdata.podcast_file);
+      var mp3FilePath = "./media/channel/bible-study/" + tdata.podcast_file;
+      const stats = fs.statSync(mp3FilePath);
       const fileSizeInBytes = stats.size;
-      var fileLengthInSeconds = await mp3Duration("./mp3/" + tdata.podcast_file, function(err, duration) {
+      var fileLengthInSeconds = await mp3Duration(mp3FilePath, function(err, duration) {
         if (err) return console.log(err.message);
         return duration;
       });
@@ -51,12 +54,12 @@ gulp.task("mp3-set-size", async function() {
 });
 
 gulp.task("mp3-write-tags", function() {
-  const dir = "./site/content/episode";
+  const dir = "./site/content/bible-study";
   fs.readdir(dir, function(err, items) {
     for (var i = 0; i < items.length; i++) {
       console.log(items[i]);
       var tdata = readEpisodeToml(path.join(dir, items[i]));
-      writeID3Tags(tdata);
+      writeID3Tags(tdata, items[i]);
     }
   });
 });
@@ -69,7 +72,41 @@ function readEpisodeToml(filePath) {
   return tdata;
 }
 
-function writeID3Tags(tdata) {
+function writeID3Tags(tdata, itemId) {
+  const infile = "./media/post/bible-study/" + tdata.podcast_file;
+  const outfile = "./media/channel/bible-study/" + tdata.podcast_file;
+  const coverfile = "./site/static/" + tdata.episode_cover;
+
+  const args = [
+    "-pcon",
+    "-pcfd", "https://www.catholicstudypodcast.com/bible-study/index.xml",
+    "-pcds", "\"Study sacred scripture with a focus on historical context and interpretation. What did the scriptures mean to those to whom it was written? That\'s the key to knowing what it means to us today!\"",
+    "-ti", "\"" + tdata.title + "\"",
+    "-t", "\"" + tdata.title + "\"",
+    "-ar", "\"" + tdata.hosts[0] + "\"",
+    "-a", "\"" + tdata.hosts[0] + "\"",
+    "-al", "\"Catholic Study Podcast\"",
+    "-b", "\"Catholic Study Podcast\"",
+    "-yr", tdata.PublishDate.getFullYear(),
+    "-y", tdata.PublishDate.getFullYear(),
+    "-c", "catholicstudypodcast.com",
+    "-g", "12",
+    "-gn", "Podcast",
+    "-co", "\"" + tdata.Description + "\"",
+    "-ur", "https://www.catholicstudypodcast.com/bible-study/" + itemId + "/",
+    "-ir", coverfile,
+    "-ad", "2",
+    "-out", outfile,
+    infile
+  ];
+
+  exec("id3edcmd " + args.join(" "), function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
+}
+
+function writeID3TagsOld(tdata) {
   const songBuffer = fs.readFileSync("./mp3/" + tdata.podcast_file);
   const coverBuffer = fs.readFileSync("./site/static/" + tdata.episode_image);
 
